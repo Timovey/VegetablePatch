ae49780e342c744184dea1bc0ab93715b786040e
Timofey Kochetkov <pochta30052001@gmail.com>
2021-09-02 17:03:20 +0400

    Добавьте файлы проекта.

---
+++ BussinesLogic.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace VegetablePatch
+{
+    public class BussinesLogic
+    {
+        private static string nameListDocs = "docs.xml";
+
+        public static List<string> GetListDocs()
+        {
+            var list = new List<string>();
+            try
+            {
+                if (!File.Exists(nameListDocs))
+                {
+                    return null;
+                }
+
+                XDocument xDocument = XDocument.Load(nameListDocs);
+                var xElements = xDocument.Root.Elements("Doc").ToList();
+                foreach (var elem in xElements)
+                {
+                    list.Add(elem.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return list;
+        }
+        public static void AddFile(string fileName)
+        {
+            try
+            {
+                string shortName = TakeNameFile(fileName);
+                if (!File.Exists(nameListDocs))
+                {
+                    var xElement = new XElement("Docs");
+                    xElement.Add(new XElement("Doc", shortName));
+                    XDocument xDocument = new XDocument(xElement);
+                    xDocument.Save(nameListDocs);
+                }
+                else
+                {
+                    bool repeatName = false;
+                    XDocument xDocument = XDocument.Load(nameListDocs);
+                    var xElements = xDocument.Root.Elements("Doc").ToList();
+                    foreach (var elem in xElements)
+                    {
+                        if (elem.Value.Equals(shortName))
+                        {
+                            repeatName = true;
+                        }
+                    }
+                    if (!repeatName)
+                    {
+                        XElement xElement = new XElement("Doc", shortName);
+                        xDocument.Root.Add(xElement);
+                        xDocument.Save(nameListDocs);
+                    }
+                }
+                File.Copy(fileName, shortName, true);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void DeleteFile(string fileName)
+        {
+            try
+            {
+                XDocument xDocument = XDocument.Load(nameListDocs);
+                var xElements = xDocument.Root.Elements("Doc").ToList();
+                int count = -1;
+                foreach (var elem in xElements)
+                {
+                    count++;
+                    if (elem.Value.Equals(fileName))
+                    {
+                        break;
+                    }
+                }
+                if (count != -1)
+                {
+                    xDocument.Root.Elements("Doc").ElementAt(count).Remove();
+                    xDocument.Save(nameListDocs);
+                    File.Delete(fileName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string TakeNameFile(string filename)
+        {
+            string[] splitString = filename.Split('\\');
+            string name = splitString.Last();
+
+            return name;
+        }
+        public static async void TakeFile(string filePath, string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new Exception("Файл не найден");
+            }
+            try
+            {
+                File.Copy(fileName, filePath, true);
+
+                Application app = new Application();
+                app.Visible = true;
+                await System.Threading.Tasks.Task.Run(() =>
+                {
+                    try
+                    {
+                        Document doc = app.Documents.Open(FileName: filePath, Visible: true);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                });
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace VegetablePatch
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void AddFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "docx|*.docx";
+
+            if ((bool)dialog.ShowDialog())
+            {
+                try
+                {
+                    BussinesLogic.AddFile(dialog.FileName);
+                    MessageBox.Show("В грядку попал новый овощ", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            Load();
+        }
+        private void Load()
+        {
+            Listbox.ItemsSource = BussinesLogic.GetListDocs();
+        }
+
+        private void Listbox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Listbox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Овощей пока еще не завезли", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "docx|*.docx";
+                dialog.FileName = Listbox.SelectedValue.ToString();
+                if ((bool)dialog.ShowDialog())
+                {
+                    BussinesLogic.TakeFile(dialog.FileName, Listbox.SelectedValue.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void DeleteFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (Listbox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Овощей пока еще не завезли", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            BussinesLogic.DeleteFile(Listbox.SelectedValue.ToString());
+            MessageBox.Show("Произошла жатва, овощ удален", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            Load();
+        }
+    }
+}
